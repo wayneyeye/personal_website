3d1201afe2d740b33cbcb8075577b92ca1d0af70
yewenhe0904 <yewenhe0904@gmail.com>
2017-03-30 00:21:15 -0500

    add Hero Slide show mode

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // is just jQuery short-hand for $(document).ready(function() { ... });
 $(function(){
+//*************************Toggle Side-Bar****************************************
 // Indicate whether sidebar is on or off
 	var sidebarOnOff = Backbone.Model.extend({
 		defaults:{
@@ -93,9 +94,120 @@ $(function(){
 			this.model.toggle();
 		}
 	});
-
-
 	var iSwitch= new sidebarOnOff();
 	var iSidebarView=new SideBarView({model: iSwitch});
 	var iAboutMe = new aboutMeView({model: iSwitch});
+
+//*************************Hero Slide Show****************************************
+	var heroImgs=[{
+	img:'img/heros/hero_slc.jpg',
+	desc:'A Glance at the Salt Lake City'
+	},
+	{
+	img:'img/heros/hero_chicago.jpg',
+	desc:'A Birdview of Downtown Chicago and Lake Michigan'
+	},
+	{
+	img:'img/heros/hero_grandcanyon.jpg',
+	desc:'Grand Canyon National Park'
+	},
+	{
+	img:'img/heros/hero_grandcanyon2.jpg',
+	desc:'Grand Canyon on Road'
+	},
+	{
+	img:'img/heros/hero_grandteton.jpg',
+	desc:'Snow Mountain in Grand Teton National Park'
+	},];
+
+	//Preload image to speed up
+	var preload = new Image();
+	heroImgs.forEach(function(hero_img){
+		preload.src = hero_img.img;
+	});
+	//Backbone model for slides
+		var heroSlidesShow = Backbone.Model.extend({
+			defaults:{
+				max: heroImgs.length,
+				index: 0
+			},
+			// Helper function for checking/unchecking a service
+			oneNext: function(){
+				var index_t = this.get("index");
+				var max_t = this.get("max");
+				index_t = ((index_t==max_t-1)?0:index_t+1);
+				this.set("index",index_t);
+			},
+			oneBack: function(){
+				var index_t = this.get("index");
+				var max_t = this.get("max");
+				index_t = ((index_t==0)?max_t-1:index_t-1);
+				this.set("index",index_t);
+			}
+		});
+//backbone view for hero image
+	var heroSlide = Backbone.View.extend({
+		tagName: 'div',
+		className: "row hero-row",
+		initialize: function(){
+			this.body= $('.hero-slide');
+			this.listenTo(iHero, 'change', this.render);
+			// console.log(this.render().el);
+			this.body.append(this.render().el);
+		},
+		render: function(){
+			// Create the HTML
+			var hero_img=heroImgs[this.model.get("index")];
+			heroLeadingHTML='<div class="col-md-12 hero"><img src="';
+			heroInnerHTML='" id="img-hero"/><div class="hero-banner"><p>';
+			heroTrailingHTML='</p></div></div>';
+			heroHTML=heroLeadingHTML+hero_img.img+heroInnerHTML+hero_img.desc+heroTrailingHTML;
+			this.$el.html(heroHTML);
+			var iLeft=new heroArrowLeft({model: iHero});
+			var iRight=new heroArrowRight({model: iHero});
+			return this;
+		}
+	});
+	var heroArrowLeft = Backbone.View.extend({
+		tagName: 'div',
+		className: "hero-left-arrow",
+		initialize: function(){
+			this.div= $('.hero-banner');
+			this.div.append(this.render().el);
+		},
+		events:{
+			'click': 'toggleSlides'
+		},
+		render: function(){
+			this.$el.html('<img src="img/icons/left.png" />');
+			return this;
+		},
+		toggleSlides: function(){
+			this.model.oneBack();
+		}
+	});
+
+	var heroArrowRight = Backbone.View.extend({
+		tagName: 'div',
+		className: "hero-right-arrow",
+		initialize: function(){
+			this.div= $('.hero-banner');
+			this.div.append(this.render().el);
+		},
+		events:{
+			'click': 'toggleSlides'
+		},
+		render: function(){
+			this.$el.html('<img src="img/icons/right.png" />');
+			return this;
+		},
+		toggleSlides: function(){
+			this.model.oneNext();
+		}
+	});
+	var iHero=new heroSlidesShow();
+	var iSlide=new heroSlide({model: iHero});
+	var iLeft=new heroArrowLeft({model: iHero});	
+	var iRight=new heroArrowRight({model: iHero});
+
 });
