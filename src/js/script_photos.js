4a682030082f8179c009c6e4ff884cdbf024ba55
yewenhe0904 <yewenhe0904@gmail.com>
2017-06-10 23:31:52 -0500

    init photo collections

--- src/js/script_photos.js
+++ src/js/script_photos.js
@@ -242,4 +242,27 @@ $(function(){
 	});
 	var iHero=new heroSlidesShow();
 	var iSlide=new heroSlide({model: iHero});
+
+	// Collections for Albums
+	var Song = Backbone.Model.extend({
+	  defaults: {
+	    name: "Not specified",
+	    artist: "Not specified"
+	  },
+	  initialize: function(){
+	      console.log("Music is the answer");
+	  }
+	});
+
+	var Album = Backbone.Collection.extend({
+	  model: Song
+	});
+
+	var song1 = new Song({ name: "How Bizarre", artist: "OMC" });
+	var song2 = new Song({ name: "Sexual Healing", artist: "Marvin Gaye" });
+	var song3 = new Song({ name: "Talk It Over In Bed", artist: "OMC" });
+
+	var myAlbum = new Album([ song1, song2, song3]);
+
+
 });
